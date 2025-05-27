e78567dd5111f1ae1dd213f497efa1497ba69614
Ezequiel Panepucci <ezequiel.panepucci@maxiv.lu.se>
2025-05-27 22:14:20 +0200

    in progress

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
+// frontend/vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    // Force development mode for devtools
+    __VUE_PROD_DEVTOOLS__: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
